89c8b757b3480649c9cbffedd0c1b446af36dab5
diakonovmakar <diakonov.makar@gmail.com>
2021-05-24 14:38:48 +0200

    cron_job updated; requirements added

--- cron_job.py
+++ cron_job.py
@@ -6,22 +6,22 @@ db_connect = sqlite3.connect('tables.sqlite')
 repository = ProductRepository(db_connect)
 offer_service = OffersServiceClient()
 
-products = repository.read_all_products()
-offers = repository.read_all_offers()
+products = repository.read_all_product_ids()
+offers = repository.read_all_offer_ids()
+offer_ids = set()
+product_ids = []
+for id in offers:
+    offer_ids.add(id[0])
+for id in products:
+    product_ids.append(id[0])
 
-offers_id = []
-for i in range(len(offers)):
-    offers_id.append(offers[i]['offer_id'])
-set_of_offers_id = set(offers_id)
-
-for i in range(len(products)):
-    product_id = products[i]['id']
-    new_offers = offer_service.products_offer(product_id)
+for id in product_ids:
+    product_id = id
+    new_offers = offer_service.product_offers(product_id)
     for offer in new_offers:
-        dict_of_data = offer
-        if dict_of_data['id'] in set_of_offers_id:
-            repository.update_offer(product_id, dict_of_data)
+        if offer['id'] in offer_ids:
+            repository.update_offer(product_id, offer)
         else:
-            repository.create_offer(product_id, dict_of_data)
+            repository.create_offer(product_id, offer)
 
 db_connect.close()
